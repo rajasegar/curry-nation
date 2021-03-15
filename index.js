ed2d866fb5034176f34207b255bfe7ccc5b8e53d
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-03-15 06:13:39 +0530

    chore: add mongoose and create documents

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ const path = require('path');
 const fastifySession = require('fastify-session');
 const fastifyCookie = require('fastify-cookie')
 const fastifyFormbody = require('fastify-formbody')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/currynation', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function() {
+});
+
+const recipeSchema = new mongoose.Schema({
+  name: String,
+  preference: String,
+  type: String,
+  cuisine: String,
+  servings: Number,
+  cookingTime: Number,
+  difficulty: Number,
+  ingredients: String
+});
+const Recipe = mongoose.model('Recipe', recipeSchema);
 
 const app = fastify({logger: true});
 
@@ -33,9 +53,25 @@ app.get('/new', async (req, res) => {
   }
 });
 
+app.post('/new', async (req, res) => {
+  const recipe = new Recipe(req.body);
+  recipe.save((err, recipe) => {
+    if(err) return console.error(err);
+    console.log('recipe created');
+  });
+  console.log(req.body);
+  res.redirect('/home');
+
+});
+
 app.get('/home', async (req, res) => {
+
+  let recipes = [];
+  recipes = await Recipe.find({});
+  console.log(recipes);
+
   if(req.session.authenticated) {
-    res.render('home');
+    res.render('home', { recipes });
   } else {
     res.redirect('/login');
   }
